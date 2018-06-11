ffa1071a055c6cf9e20b4f415b008ac101be1caf
Daniel <dfdazac@gmail.com>
2018-06-11 10:50:09 +0200

    Added usage

--- nb2page.sh
+++ nb2page.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+if [ "$1" == "-h" ]; then
+  echo "Usage: `basename $0` NOTEBOOK PAGE_ROOT
+    where
+    - NOTEBOOK is the path of the notebook
+    - PAGE_ROOT is the root directory of the GitHub page"
+  exit 0
+fi
+
 jupyter nbconvert --to markdown "$1"
 python nb2page.py "$1" "$2"
